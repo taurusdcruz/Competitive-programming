8d70cbbd24bdf1d7573081ec7cd25fe92794f5a3
taurusdcruz <taurusdcruz1@yahoo.com>
2022-09-21 05:54:03 +0530

    add template for python

---
+++ src/Good code - python/template-python.py
@@ -0,0 +1,38 @@
+import sys
+import functools as _ft
+import itertools as _it
+import operator as _op
+import bisect as _bsct
+import collections as _cllctn
+import heapq as _hpq
+sys.setrecursionlimit(10**6)
+dd = _cllctn.defaultdict
+bl = _bsct.bisect_left
+br = _bsct.bisect_right
+
+MOD = int(1e9+7)
+DONT_LOG = 0
+
+def log(level=0):
+    prefix = "-".join(["|" for _ in range(level)])
+    return lambda *args: None if (DONT_LOG)else print(prefix, *args)
+def exists(x):
+    return x is not None
+def in_int():
+    return int(input())
+def in_arr_int():
+    return list(map(int, input().split()))
+def min_excluding_none(*args):
+    filtered_args = filter(lambda x: x is not None, args)
+    res = min(filtered_args, default=None)
+    return res
+
+def solve(t)
+    print("Case #{}: {}".format(t+1, res))
+
+T = int(input())
+for t in range(T):
+    try:
+        solve(t)
+    except Exception as e:
+        print(e)
