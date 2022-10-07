e38e32e929cba223b22cb7f1e7d564028bfcda21
taurusdcruz <taurusdcruz1@yahoo.com>
2022-10-07 21:11:05 +0530

    update min_excluding_none

--- src/Good code - python/template-python.py
+++ src/Good code - python/template-python.py
@@ -5,30 +5,45 @@ import operator as _op
 import bisect as _bsct
 import collections as _cllctn
 import heapq as _hpq
+
 sys.setrecursionlimit(10**6)
 dd = _cllctn.defaultdict
 bl = _bsct.bisect_left
 br = _bsct.bisect_right
 
-MOD = int(1e9+7)
+MOD = int(1e9 + 7)
 DONT_LOG = 0
+
 
 def log(level=0):
     prefix = "-".join(["|" for _ in range(level)])
-    return lambda *args: None if (DONT_LOG)else print(prefix, *args)
+    return lambda *args: None if (DONT_LOG) else print(prefix, *args)
+
+
 def exists(x):
     return x is not None
+
+
 def in_int():
     return int(input())
+
+
 def in_arr_int():
     return list(map(int, input().split()))
+
+
+def args_not_none(*args):
+    return filter(lambda x: x is not None, args)
+
+
 def min_excluding_none(*args):
-    filtered_args = filter(lambda x: x is not None, args)
-    res = min(filtered_args, default=None)
+    res = min(args_not_none(args), default=None)
     return res
 
-def solve(t)
-    print("Case #{}: {}".format(t+1, res))
+
+def solve(t):
+    print("Case #{}: {}".format(t + 1, res))
+
 
 T = int(input())
 for t in range(T):
